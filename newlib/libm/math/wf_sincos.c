305f62ec2ef5e5cea7d19370313728225434e5e7
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2024-05-24 09:19:03 +0800

    newlib: disable optmisation for sincos
    Signed-off-by: p-szafonimateusz <p-szafonimateusz@xiaomi.com>

--- newlib/libm/math/wf_sincos.c
+++ newlib/libm/math/wf_sincos.c
@@ -3,8 +3,19 @@
 #include "fdlibm.h"
 #if __OBSOLETE_MATH
 
+/* Disable sincos optimization for all functions in this file,
+ * otherwise gcc would generate infinite calls.
+ * Refer to gcc PR46926.
+ * -fno-builtin-sin or -fno-builtin-cos can disable sincos optimization,
+ * but these two options do not work inside optimize pragma in-file.
+ * Thus we just enforce -O0 when compiling this file.
+ */
+
 #include <errno.h>
 
+#ifdef __GNUC__
+__attribute__((optimize("O0")))
+#endif
 #ifdef __STDC__
 	void sincosf(float x, float *sinx, float *cosx)
 #else
@@ -20,6 +31,9 @@
 
 #ifdef _DOUBLE_IS_32BITS
 
+#ifdef __GNUC__
+__attribute__((optimize("O0")))
+#endif
 #ifdef __STDC__
 	void sincos(double x, double *sinx, double *cosx)
 #else
